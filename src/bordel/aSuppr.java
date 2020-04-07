9479f9265ce1eafe9a537b9a8f936a318624bd7d
laenNoCode <youen.froger2@gmail.com>
2020-04-07 20:15:05 +0200

    mini test piano

--- src/bordel/aSuppr.java
+++ src/bordel/aSuppr.java
@@ -1,17 +1,50 @@
 package bordel;
 
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.sound.midi.*;
+import javax.swing.JFrame;
 
 
 public class aSuppr {
 
 	public static void main(String[] args) throws MidiUnavailableException, InvalidMidiDataException, InterruptedException {
-		Receiver rcvr = MidiSystem.getReceiver();
-		ShortMessage sm = new ShortMessage();
-		sm.setMessage(ShortMessage.NOTE_ON,0,57,127);
-		rcvr.send(sm, -1);
-		Thread.sleep(2000);
+		JFrame jeff = new JFrame();
+		jeff.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jeff.setVisible(true);
+		jeff.setSize(500, 500);
+		String keys = "azertyuiop";
+		jeff.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				try {
+				Receiver rcvr = MidiSystem.getReceiver();
+				ShortMessage sm = new ShortMessage();
+				sm.setMessage(ShortMessage.NOTE_ON,0,57+keys.indexOf(e.getKeyChar()),127);
+				rcvr.send(sm, -1);
+				//do do# re re# mi fa fa# sol sol# la la# si 
+				}catch(Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		
 	}
 
 }
